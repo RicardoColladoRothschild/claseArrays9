e5b0e4ecbf5caeb627cbd165475c389cc6779094
ricardocollado <ricardocolladorothschild@gmail.com>
2023-04-20 20:28:16 -0400

    creamos practica de arrays basico

---
+++ Arrays.java
@@ -0,0 +1,12 @@
+public class Arrays{
+    public static void main(String[] args){
+
+        String[] nombres = {"ricardo", "felixon"};
+        System.out.println(nombres[1]);
+        System.out.println(nombres[0]);
+        
+        
+    }
+
+}
+/**Ir a extensiones e instalar code runner, java debugger, Debugger for Java */
