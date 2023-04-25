cf0ac471f87c7d77ce22cab7b794ee0d24474d0a
ricardocollado <ricardocolladorothschild@gmail.com>
2023-04-25 18:58:20 -0400

    erase content on arrays to start over

--- Arrays.java
+++ Arrays.java
@@ -1,13 +1,1 @@
-public class Arrays{
-    public static void main(String[] args){
 
-        String[] nombres = {"ricardo", "felixon"};
-        int[] numeros = {1,2,3,4,5};
-        System.out.println(nombres[1]);
-        System.out.println(nombres[0]);
-        
-        
-    }
-
-}
-/**Ir a extensiones e instalar code runner, java debugger, Debugger for Java */
