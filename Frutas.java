521f771e0ab754e0c37dbcf44f1d18b18e3c333c
Josefinagarcaa <josefina.sanchezgarcaa@gmail.com>
2023-04-27 18:11:42 -0500

    Practica Frutas

---
+++ Frutas.java
@@ -0,0 +1,12 @@
+public class Frutas{
+    public static void main(String[] args){
+    String[] Frutas = {"Mango","Fresa","Pera","Chinola","Pitajaya"};
+
+    System.out.println(Frutas[0]);
+    System.out.println(Frutas[1]);
+    System.out.println(Frutas[2]);
+    System.out.println(Frutas[3]);
+    System.out.println(Frutas[4]);
+
+    }
+}
