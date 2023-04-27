63b704012a221b6c87c35c3b1ce3116261e548dc
Josefinagarcaa <josefina.sanchezgarcaa@gmail.com>
2023-04-27 16:58:46 -0500

    Hice clase arraysNombre

---
+++ ArraysNombre.java
@@ -0,0 +1,10 @@
+public class ArraysNombre{
+
+    public static void main(String[] args){
+    String[] nombres = {"Juan","Pedro","Martin","Daniel","Roberto","Jesus","Pancracio","Tiburcio","Joshua","Esmil"};
+
+    System.out.println(nombres[4]);
+
+
+    }
+}
