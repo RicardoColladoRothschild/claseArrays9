93c70a8eefac6126edeb5db122f71cc17f4ceb88
Josefinagarcaa <josefina.sanchezgarcaa@gmail.com>
2023-05-03 20:06:03 -0500

    Practicando ciclos while

---
+++ ciclosWhils.java
@@ -0,0 +1,19 @@
+public class ciclosWhils{
+    public static void main(String[] args){
+
+        boolean hacerCompra = true;
+        boolean usarTarjeta = false;
+
+        while (hacerCompra){
+                System.out.println("Puedes hacer compra");
+                hacerCompra = false;
+            int i = 0; //no iniciaste el int
+        do{
+                System.out.println("Puedes usar la tarjeta");
+                usarTarjeta = false;
+
+            i++;
+            }break;
+        }
+    }
+}
